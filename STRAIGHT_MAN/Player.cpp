9c77e43a1423140893bd6637ff56009625af374e
ssb2ss <44504789+ssb2ss@users.noreply.github.com>
2019-07-25 22:55:49 +0900

    Smooth Moving (Player)

--- STRAIGHT_MAN/Player.cpp
+++ STRAIGHT_MAN/Player.cpp
@@ -3,7 +3,7 @@
 
 Player::Player()
 {
-
+	moveTime = 100;
 }
 
 Player::~Player()
@@ -19,17 +19,78 @@ void Player::Render() {
 }
 
 void Player::Move(int destX, int destY) {
-	
-	setBackgroundColor(ColorBlack);
-	gotoxy(2 + x * 2, 1 + y);
-	std::cout << "  ";
+
+	if (x != destX) {
+		if (x > destX) {
+			setBackgroundColor(ColorLightRed);
+			for (int i = x; i >= destX; i--) {
+				gotoxy(2 + i * 2, 1 + y);
+				std::cout << "  ";
+			}
+			Sleep(moveTime);
+			int diff = x - destX;
+			setBackgroundColor(ColorBlack);
+			for (int i = x; i > destX; i--) {
+				gotoxy(2 + i * 2, 1 + y);
+				std::cout << "  ";
+				Sleep(moveTime / diff);
+			}
+		}
+		else {
+			setBackgroundColor(ColorLightRed);
+			for (int i = x; i <= destX; i++) {
+				gotoxy(2 + i * 2, 1 + y);
+				std::cout << "  ";
+			}
+			Sleep(moveTime);
+			int diff = destX - x;
+			setBackgroundColor(ColorBlack);
+			for (int i = x; i < destX; i++) {
+				gotoxy(2 + i * 2, 1 + y);
+				std::cout << "  ";
+				Sleep(moveTime / diff);
+			}
+		}
+	}
+	else if (y != destY) {
+		if (y > destY) {
+			setBackgroundColor(ColorLightRed);
+			for (int i = y; i >= destY; i--) {
+				gotoxy(2 + x * 2, 1 + i);
+				std::cout << "  ";
+			}
+			Sleep(moveTime);
+			int diff = y - destY;
+			setBackgroundColor(ColorBlack);
+			for (int i = y; i > destY; i--) {
+				gotoxy(2 + x * 2, 1 + i);
+				std::cout << "  ";
+				Sleep(moveTime / diff);
+			}
+		}
+		else {
+			setBackgroundColor(ColorLightRed);
+			for (int i = y; i <= destY; i++) {
+				gotoxy(2 + x * 2, 1 + i);
+				std::cout << "  ";
+			}
+			Sleep(moveTime);
+			int diff = destY - y;
+			setBackgroundColor(ColorBlack);
+			for (int i = y; i < destY; i++) {
+				gotoxy(2 + x * 2, 1 + i);
+				std::cout << "  ";
+				Sleep(moveTime / diff);
+			}
+		}
+	}
+
+	while (_kbhit()) {	//
+		_getch();		// 버퍼 초기화
+	}					//
 
 	x = destX;
 	y = destY;
-
-	setBackgroundColor(ColorLightRed);
-	gotoxy(2 + x * 2, 1 + y);
-	std::cout << "  ";
 
 	setBackgroundColor(ColorBlack);
 
--- STRAIGHT_MAN/Player.h
+++ STRAIGHT_MAN/Player.h
@@ -3,6 +3,7 @@ class Player
 {
 public:
 	int x, y;
+	int moveTime;
 
 	Player();
 	~Player();
